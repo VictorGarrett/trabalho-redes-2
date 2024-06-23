28a1937d1a6890479a040c27b9f0e58549adacc9
VictorGarrett <victor_garrett@hotmail.com>
2024-06-23 20:28:44 -0300

    removed cobol

--- client.py
+++ client.py
@@ -1,11 +1,47 @@
 import socket
+import threading
+import hashlib
+import os
 
 HOST = "127.0.0.1"  # The server's hostname or IP address
 PORT = 65433  # The port used by the server
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b"Hello, world")
-    data = s.recv(1024)
+def print_received(messages, s):
+    while True:
+        data = s.recv(1024)
+        if data[0:4] == b'text':
+            print(data[4:].decode('ascii'))
+        elif data[0:4] == b'fbeg':
+            md5 = hashlib.md5()
+            filename = data[4:]
+            recvfile = open(filename, 'wb')
+        elif data[0:4] == b'fprt':
+            recvfile.write(data[4:])
+            md5.update(data[4:]) 
+        elif data[0:4] == b'fend':
+            received_hash = data[4:]
+            calc_hash = md5.digest()
 
-print(f"Received {data!r}")
+            recvfile.close()
+            if received_hash != calc_hash:
+                print('Failed to receive file!')
+                os.remove(filename)
+            
+
+
+messages = []
+
+
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+
+    
+    text = ''
+    s.connect((HOST, PORT))
+    t = threading.Thread(target=print_received, args=(messages, s))
+    t.start()
+    while text != 'exit':
+        text = input()
+        s.sendall(str.encode(text))
+
+#print(f"Received {data!r}")
--- server.py
+++ server.py
@@ -1,32 +1,110 @@
 import socket
 import threading
+import sys
+import hashlib
+import os
 
-HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
-PORT = 65433  # Port to listen on (non-privileged ports are > 1023)
+HOST = "127.0.0.1" 
+PORT = 65433  
 
-def on_new_client(clientsocket,addr):
-    
+def listen(serversocket, stop_event):
+    while not stop_event.is_set():
+        try:
+            c, addr = serversocket.accept()
+            c.settimeout(1)
+        except Exception as e:
+            #print(f'exception on listen {e}')
+            continue
+
+        socket_map[addr] = c
+        t = threading.Thread(target=client_recieve, args=(c, addr, messages, socket_map, stop_event))
+        t.start()
+
+def send_file(clientsocket, filename):  
+    #print(f"sendfile {filename.decode('ascii')}---")
+    try:
+        with open(filename.decode('ascii'), 'rb') as file:
+            clientsocket.sendall(b'fbeg' + filename)
+            md5 = hashlib.md5()
+            chunk = file.read(1020)
+            while chunk:
+                md5.update(chunk) 
+                clientsocket.sendall(b'fprt' + chunk)
+                chunk = file.read(1020)
+                
+        clientsocket.sendall(b'fend' + md5.digest())
+    except:
+        clientsocket.sendall(b'text' + b'NO SUCH FILE!')
+
+def send(messages, socket_map, stop_event):
+    while not stop_event.is_set():
+        if len(messages) > 0:
+            msg = messages.pop()
+            for addr, socket in socket_map.items():
+                if msg[0] != addr:
+                    socket.sendall(b'text'+ msg[1])
+
+def client_recieve(clientsocket, addr, messages, socket_map, stop_event):
+    clientsocket.setblocking(1)
     received_text = ''
 
-    while received_text != 'exit'
-    msg = clientsocket.recv(1024)
-    #do some checks and if msg == someWeirdSignal: break:
-    print (f"{addr} >>  {msg}")
-    #Maybe some code to compute the last digit of PI, play game or anything else can go here and when you are done.
-    clientsocket.sendall(msg)
+    while received_text != 'exit' and not stop_event.is_set():
+        try:
+            msg = clientsocket.recv(1024)
+            if len(msg) > 0:
+                #print (f"{addr} >>  {msg}")
+
+                if msg[0:7] == b'arquivo':
+                    send_file(clientsocket, msg[8:])
+                elif msg != 'exit':
+                    messages.append((addr, msg))
+                    print(msg.decode('ascii'))
+                #clientsocket.sendall(msg)
+                #print(messages)
+            else:
+                break
+        except Exception as e:
+            #print(f'exception on recv: {e}')
+            continue
+    #print("closing socket")
     clientsocket.close()
+    del socket_map[addr]
 
-s = socket.socket()         # Create a socket object
-
-
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)         # Create a socket object
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 s.bind((HOST, PORT))        # Bind to the port
 s.listen()                 # Now wait for client connection.
-while True:
-   c, addr = s.accept()     # Establish connection with client.
-   t = threading.Thread(target=on_new_client, args=(c, addr))
-   t.start()
-   #Note it's (addr,) not (addr) because second parameter is a tuple
-   #Edit: (c,addr)
-   #that's how you pass arguments to functions when creating new threads using thread module.
+s.settimeout(1)
+
+stop_event = threading.Event()
+
+messages = []
+socket_map = {}
+
+sender_thread = threading.Thread(target=send, args=(messages, socket_map, stop_event))
+sender_thread.start()
+
+running = True
+
+listen_thread = threading.Thread(target=listen, args=(s, stop_event))
+listen_thread.start()
+
+while running:
+   
+    try:
+        x = input()  
+        #print(x)
+        if x == 'exit':
+            running = False
+        else:
+            messages.append((None, str.encode(x)))
+    except Exception as e:
+        #print(f'exception on main: {e}')
+        running = False
+
+
+stop_event.set()
+listen_thread.join()
+#print('thread joined')
 s.close()
